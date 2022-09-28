2be034363cbb153d3a06a4e5d304aeaffc8c14d1
honordevop <ogunladestephen20@gmail.com>
2022-09-28 14:14:48 +0100

    Code: implemented a function to save user selected stock into localStorage for synchronization

--- src/context/watchListContext.jsx
+++ src/context/watchListContext.jsx
@@ -3,7 +3,19 @@ import { createContext, useState } from "react";
 export const WatchListContext = createContext()
 
 export const WatchListContextProvider = (props) => {
-    const [watchList, setWatchList] = useState(['GOOGL', 'MSFT', 'AMZN']);
+    // const [watchList, setWatchList] = useState(getWatchList() || ['GOOGL', 'MSFT', 'AMZN']);
+
+    //function to get watchlist content from localstorage if it exist
+    const getWatchList = ()=> {
+        let watchList = localStorage.getItem('watchlist')
+        if (watchList) {
+            watchList = JSON.parse(localStorage.getItem('watchlist'))
+            return watchList
+        }
+    }
+    const [watchList, setWatchList] = useState(getWatchList() || ['GOOGL', 'MSFT', 'AMZN']);
+
+    localStorage.setItem('watchlist', JSON.stringify(watchList))
 
     const addStock = (stock)=> {
         if (watchList.indexOf(stock) === -1) {
